b6d4ad582427c04b25ef1e865f8cb534acaa75a0
Fernando Moreira <marketingfuturaclimatizacao@gmail.com>
2026-01-13 18:13:23 -0300

    Correcao next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Cria a pasta "out" para o Render
+  images: {
+    unoptimized: true, // Necessário para imagens funcionarem no modo estático
+  },
 };
 
 export default nextConfig;
